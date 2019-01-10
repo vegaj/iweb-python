78ff76c182cae8de3549a1c6a1c9ce47e3bc0f37
Alvaro Reina <arb952@gmail.com>
2019-01-10 14:48:54 +0100

    Login y logout con session

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-from views import NewSerie, ListSeries, ShowSerie, EditSerie, DeleteSerie, NewSketch, EditSketch, DeleteSketch
+from views import NewSerie, ListSeries, ShowSerie, EditSerie, DeleteSerie, NewSketch, EditSketch, DeleteSketch, Login, Logout
 import webapp2
+
+config = {'webapp2_extras.sessions': {
+    'secret_key': 'my-super-secret-key',
+}}
 
 app = webapp2.WSGIApplication([
         ('/', ListSeries),
+        ('/login', Login),
+        ('/logout', Logout),
         ('/sketches/new', NewSketch),
         ('/sketches/edit/([\d]+)', EditSketch),
         ('/sketches/delete/([\d]+)', DeleteSketch),
@@ -12,4 +18,5 @@ app = webapp2.WSGIApplication([
         ('/series/edit/([\d]+)', EditSerie),
         ('/series/delete/([\d]+)', DeleteSerie),
         ],
+        config=config,
         debug=True)
--- src/views.py
+++ src/views.py
@@ -4,6 +4,7 @@ import webapp2
 import jinja2
 
 from google.appengine.ext import db
+from webapp2_extras import sessions
 
 from models import Serie, Sketch
 
@@ -14,12 +15,29 @@ jinja_environment = \
 
 class BaseHandler(webapp2.RequestHandler):
 
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)  # dispatch the main handler
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
     def render_template(
             self,
             filename,
             template_values,
             **template_args
     ):
+        template_values['logged'] = self.session.get('logged')
         template = jinja_environment.get_template(filename)
         self.response.out.write(template.render(template_values))
 
@@ -247,3 +265,28 @@ class DeleteSketch(BaseHandler):
 
     def get(self, add_id):
         webapp2.redirect("/")
+
+
+class Login(BaseHandler):
+
+    def get(self):
+        if self.session.get('logged'):
+            return self.redirect("/")
+        else:
+            return self.render_template("login.html", {})
+
+    def post(self):
+        self.session['user_name'] = self.request.get('user_name')
+        self.session['user_email'] = self.request.get('user_email')
+        self.session['logged'] = 'true'
+        return self.redirect("/")
+
+
+class Logout(BaseHandler):
+
+    def get(self):
+        if self.session.get('logged'):
+            del self.session['logged']
+            del self.session['user_name']
+            del self.session['user_email']
+        return self.redirect("/")
