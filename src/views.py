e87180f46deb1a599a5c0037f326f5f7d2f5dd3d
José Vega Moreno <jvegamoreno@outlook.com>
2019-01-10 14:28:02 +0100

    Merge editar sketch.

--- src/views.py
+++ src/views.py
@@ -226,8 +226,58 @@ class NewSketch(BaseHandler):
 
 class EditSketch(BaseHandler):
 
-    def get(self, add_id):
-        webapp2.redirect("/")
+    def get(self, sketch_id):
+        iden = int(sketch_id)
+        sketch = db.get(db.Key.from_path('Sketch', iden))
+        error = None
+        p = {
+            'title': sketch.title,
+            'createdAt': sketch.createdAt,
+            'score': sketch.score,
+        }
+
+        self.render_template('sketches/edit.html', p)
+
+    def post(self, sketch_id):
+
+        error = None
+        p = {
+            'title': self.request.get('inputTitle'),
+            'createdAt': self.request.get('inputCreatedAt'),
+            'score': self.request.get('inputScore'),
+
+        }
+
+        # Input validation
+        if not p['title']:
+            error = 'El titulo esta vacio';
+
+        try:
+            p['createdAt'] = date(p['createdAt'])
+        except ValueError:
+            error = 'La fecha esta vacia'
+
+        try:
+            # Cambiar el modelo de int a float
+            p['score'] = long(p['score'])
+        except ValueError:
+            error = 'La puntuacion debe ser un numero'
+
+        if error:
+            p['error'] = error
+            return self.render_template("sketches/edit.html", p)
+
+        try:
+            iden = int(sketch_id)
+            sketch = db.get(db.Key.from_path('Sketch', iden))
+            sketch.title = p['title']
+            sketch.createdAt = p['createdAt']
+            sketch.score = p['score']
+            sketch.put()
+            return webapp2.redirect('/')
+        except Exception as e:
+            p['error'] = 'No se pudo editar por {}'.format(e.message)
+            return self.render_template("sketches/edit.html", p)
 
 
 class DeleteSketch(BaseHandler):
